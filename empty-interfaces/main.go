a7de2d7f880a4b012dbb58963992c1ff204a8ea6
Jhon Guerrero <jhoguer@gmail.com>
2020-08-22 10:54:07 -0500

    Con el type asertion podemos saber que tipo de dato fue enviado a la interface vacia y asi lo podemos controlar

--- empty-interfaces/main.go
+++ empty-interfaces/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // exampler es una interface vacia porque no se implemeta ningun metodo
 // y sirve para recibir tipos de datos desconocidos
@@ -10,12 +13,22 @@ type exampler interface {
 
 func wrapper(i interface{}) {
 	fmt.Printf("Valor: \t %v\nTipo: \t %T\n\n", i, i)
+
+	// atreves del type asertion podemos validar que tipo de dato recibimos
+	// en este caso validamos el tipo concreto que trae la interfaz vacia
+	// v es el valor que se esta mandando en la interface vacia
+	// ok es un booleano que es true si la interface vacia es un string
+	v, ok := i.(string)
+	if ok {
+		fmt.Printf("\t%s\n", strings.ToUpper(v))
+	}
 }
 
 func main() {
 	// var e exampler
 	// e.x()
 	wrapper(12)
+	wrapper("Betty")
 	wrapper(13.67)
 	wrapper(false)
 	wrapper("Jhon")
