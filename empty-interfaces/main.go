cc5a4cddc0ed7fecad0ff536260e0e47eae36d95
Jhon Guerrero <jhoguer@gmail.com>
2020-08-22 11:56:30 -0500

    usando type assertion para operar una variable dependiendo de su tipo de dato

--- empty-interfaces/main.go
+++ empty-interfaces/main.go
@@ -12,15 +12,26 @@ type exampler interface {
 }
 
 func wrapper(i interface{}) {
-	fmt.Printf("Valor: \t %v\nTipo: \t %T\n\n", i, i)
 
-	// atreves del type asertion podemos validar que tipo de dato recibimos
+	// Atreves del type asertion podemos validar que tipo de dato recibimos
 	// en este caso validamos el tipo concreto que trae la interfaz vacia
 	// v es el valor que se esta mandando en la interface vacia
 	// ok es un booleano que es true si la interface vacia es un string
-	v, ok := i.(string)
-	if ok {
-		fmt.Printf("\t%s\n", strings.ToUpper(v))
+	// v, ok := i.(string)
+	// if ok {
+	// 	fmt.Printf("\t%s\n", strings.ToUpper(v))
+	// }
+
+	// i.(type) devuelve el tipo de dato de la interface
+	switch v := i.(type) {
+	case string:
+		fmt.Printf("%s\t%s\n", v, strings.ToUpper(v))
+	case int:
+		fmt.Printf("%v\t%v\n", v, v*2)
+	case bool:
+		fmt.Printf("%v\t%v\n", v, !v)
+	default:
+		fmt.Printf("Valor: \t %v\nTipo: \t %T\n\n", i, i)
 	}
 }
 
